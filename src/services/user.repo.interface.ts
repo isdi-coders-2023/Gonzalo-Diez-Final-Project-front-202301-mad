4b5aa70a63edf595bb1faa5171528a3834bd1b7b
gonzu <gonzalodiezdavila@gmail.com>
2023-03-16 23:39:24 +0100

    🌱 Add user repo code & testing

---
+++ src/services/user.repo.interface.ts
@@ -0,0 +1,6 @@
+import { User } from "../models/user";
+
+export interface URepo<P> {
+  create(info: Partial<User>, path: string): Promise<P>;
+  update(info: Partial<User>, path: string, token: string): Promise<P>;
+}
